38cfd67cc0a658a84cac604949b43c0edcf48b37
Pankaj Kumar <pankjku5698@gmail.com>
2026-09-05 16:30:56 +0530

    Update vite.config & package.json

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Student_Focus_Flow/',
+  base: '/STUDENT_FOCUS_FLOW/',
   plugins: [react(), tailwindcss()],
 })
