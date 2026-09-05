674592e30fae13309d283da9f663d41fd8ae0441
Pankaj Kumar <pankjku5698@gmail.com>
2026-09-05 16:41:05 +0530

    Fix vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/STUDENT_FOCUS_FLOW/',
   plugins: [react(), tailwindcss()],
 })
